4b3ef1066c9f086438632ebbf440506af89dbb73
Paul Withers <pwithers@intec.co.uk>
2013-05-31 13:46:05 +0100

    Fixing issue with value for Message column in Open Log Database view Adding Java example and class Fixing missing errors block

---
+++ OpenLogJava/WebContent/WEB-INF/demo/com/paulwithers/tests/OpenLogTest.java
@@ -0,0 +1,37 @@
+package com.paulwithers.tests;
+
+//	Copyright 2013 Paul Withers Licensed under the Apache License, Version 2.0
+//	(the "License"); you may not use this file except in compliance with the
+//	License. You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+//	or agreed to in writing, software distributed under the License is distributed
+//	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//	express or implied. See the License for the specific language governing
+//	permissions and limitations under the License
+
+import lotus.domino.Database;
+import lotus.domino.Document;
+import lotus.domino.View;
+
+import com.paulwithers.openLog.OpenLogItem;
+import com.timtripcony.util.JSFUtil;
+
+public class OpenLogTest {
+	public OpenLogTest() {
+
+	}
+
+	public static String demoFail() {
+		String retVal = "";
+		try {
+			Database currDb = JSFUtil.getCurrentDatabase();
+			View tmpView = currDb.getView("Fail");
+			Document errDoc = tmpView.getFirstDocument();
+		} catch (Exception e) {
+			OpenLogItem.logError(e);
+			OpenLogItem.addFacesMessage(null, e.getMessage());
+		}
+		return retVal;
+	}
+}
